ea7ec9b76b72cc5a68abe0db0f59c781cfd5671e
Rohith Reddy <samarohitreddy@gmail.com>
2026-02-16 19:51:04 -0500

    test run

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import App from './App';
+import App from './App.js';
 
 // Create a manual mock control object
 const mockApi = {
